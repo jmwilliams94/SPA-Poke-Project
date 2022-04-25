293522e023dfcb383b90e3d9ad4f952238352d0e
Jordan <jmwilliams94@icloud.com>
2022-04-25 19:44:40 +1000

    Created for loop fetch for pokemon with selected data

--- js/index.js
+++ js/index.js
@@ -1,15 +1,31 @@
-const gen1 = 1<150
-const get2 = 151<250
-const gen3 = 251<385
-const allPokemon = fetchPokemon();
+const gen1 = 150
+// const get2 = 151<250
+// const gen3 = 251<385
 
+const allPokemon = async (id) => {
+  const promises = [];
+  for(let i=1; i<=gen1; i++) {
+    const res = await fetch(`https://pokeapi.co/api/v2/pokemon/${i}`);
+        const data = await res.json();
 
-async function fetchPokemon() {
-    const response = await fetch('https://pokeapi.co/api/v2/pokemon?limit=100000&offset=0');
-    const result = await response.json();
-    return result;
-  }
-  fetchPokemon().then(result => {
-    return result;
-  });
+          const gen1pokemon = {
+            name: data.name,
+            id: data.id,
+            image: data.sprites['front_default'],
+            type: data.types
+            .map((type) => type.type.name)
+            .join(', ')
+          };
+          console.log(gen1pokemon);
+        } 
+};
+allPokemon();
 
+// const fetchGeneration1 = async () => {
+//   for(let i=1; i<=gen1; i++) {
+//     const gen1poke = await allPokemon(i);
+//     console.log(gen1poke)
+//   }
+// }
+// fetchGeneration1();
+// // Add in catch

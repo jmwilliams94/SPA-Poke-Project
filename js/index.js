4f3bd5cd1ce06b34d51abf4409a786deaf75594e
Jordan <jmwilliams94@icloud.com>
2022-05-01 15:09:06 +1000

    Added search bar filter

--- js/index.js
+++ js/index.js
@@ -1,95 +1,198 @@
-const gen1 = 151
-const gen2 = 251
-const gen3 = 385
 const pokedex = document.getElementById('pokedex');
-console.log(pokedex);
 
-const genOnePokemon = () => {
-  const promises = [];
-  for(let i=1; i<=gen1; i++) {
-    const url = `https://pokeapi.co/api/v2/pokemon/${i}`
-    promises.push(fetch(url).then((res) => res.json())); //Stores all promises(pokemon) into an array.
-  } 
-  Promise.all(promises).then((results) => { //Converts it to a format that loads all Pokemon at one time as opposed to having each loop run one after the other.
-    const pokemon1 = results.map((data) => ({
-            name: data.name,
-            id: data.id,
-            image: data.sprites['front_default'],
-            shiny: data.sprites['front_shiny'],
-            type: data.types.map((type) => type.type.name).join(', ')
-          }));
-          displayPokemon(pokemon1);
-  });
-};
+const typeOne = document.getElementById('type1selector');
 
-const displayPokemon = (pokemon) => {
+const renderPokemon = (pokemon) => {
   console.log(pokemon);
-  const pokemonHTMLstring = pokemon.map( pokeman => `
-  <li class ="card">
-    <img class="card-image" src="${pokeman.image}"/>
-    <h2 class="card-title">${pokeman.id}. ${pokeman.name}</h2>
-    <p class="type-sub">Type: ${pokeman.type}</p>
-  </li>
-  `
-  ).join('');
+  // console.log(pokemon[1].name);
+  const pokemonHTMLstring = pokemon
+    .map((pokeman) => {
+      let [classOne, classTwo] = [pokeman.type, null];
+
+      if (pokeman.type.split(' ').length > 1) {
+        [classOne, classTwo] = pokeman.type.split(' ');
+        // classOne = pokeman.type.split(' ')[0];
+        // classTwo = pokeman.type.split(' ')[1];
+      }
+
+      let typeHtmlString = `<span>Type:</span><p  class="type-sub pill ${classOne}"> ${pokeman.type}</p>`;
+      if (classTwo) {
+        typeHtmlString = `<span>Type:</span><span class="type-sub pill ${classOne}"> ${classOne}</span><span class="pill type-sub ${classTwo}"> ${classTwo}</span>`;
+      }
+
+      return ` <li class ="card">
+                <img class='card-image poke-image' onmouseover="this.src='${pokeman.shiny}'" onmouseout="this.src='${pokeman.image}'"  src='${pokeman.image}' />
+                <h2 class="card-title">${pokeman.id}.${pokeman.name}</h2>
+                ${typeHtmlString}
+                </li>`;
+    })
+    .join('');
+
   pokedex.innerHTML = pokemonHTMLstring;
 };
+//
 
+// GEN ONE
+// ----------------------------------------------------------------------------------------
+const genOnePokemon = () => {
+  const promises = [];
+  for (let i = 1; i <= 151; i++) {
+    const url = `https://pokeapi.co/api/v2/pokemon/${i}`;
+    promises.push(fetch(url).then((res) => res.json()));
+    // Stores all promises(pokemon) into an array.
+  }
+  Promise.all(promises).then((results) => {
+    // Converts it to a format that loads all Pokemon at one time as opposed to having
+    // each loop run one after the other.
+    const pokemon1 = results.map((data) => ({
+      name: data.name,
+      id: data.id,
+      image: data.sprites.front_default,
+      shiny: data.sprites.front_shiny,
+      type: data.types.map((type) => type.type.name).join(' '),
+    }));
 
+    // if (typeOne != 'all') {
+
+    // }
+    renderPokemon(pokemon1);
+  });
+};
 genOnePokemon();
 
-const genTwoPokemon = () => {
+// GEN ONE END
+// ----------------------------------------------------------------------------------------
+const genTwoPokemon = (typeOne) => {
+  console.log('Gen TWO pokemon!');
   const promises = [];
-  for(let i=152; i<=gen2; i++) {
-    const url = `https://pokeapi.co/api/v2/pokemon/${i}`
-    promises.push(fetch(url).then((res) => res.json())); //Stores all promises(pokemon) into an array.
-  } 
-  Promise.all(promises).then((results) => { //Converts it to a format that loads all Pokemon at one time as opposed to having each loop run one after the other.
+  for (let i = 152; i <= 252; i++) {
+    const url = `https://pokeapi.co/api/v2/pokemon/${i}`;
+    promises.push(fetch(url).then((res) => res.json()));
+    // Stores all promises(pokemon) into an array.
+  }
+  Promise.all(promises).then((results) => {
+    // Converts it to a format that loads all Pokemon at one time as opposed
+    //  to having each loop run one after the other.
     const pokemon2 = results.map((data) => ({
-            name: data.name,
-            id: data.id,
-            image: data.sprites['front_default'],
-            shiny: data.sprites['front_shiny'],
-            type: data.types.map((type) => type.type.name).join(', ')
+      name: data.name,
+      id: data.id,
+      image: data.sprites.front_default,
+      shiny: data.sprites.front_shiny,
+      type: data.types.map((type) => type.type.name).join(' '),
     }));
-    console.log(pokemon2)
+
+    renderPokemon(pokemon2);
   });
 };
-
-genTwoPokemon();
 
 const genThreePokemon = () => {
+  console.log('Gen THREE pokemon!');
   const promises = [];
-  for(let i=252; i<=gen3; i++) {
-    const url = `https://pokeapi.co/api/v2/pokemon/${i}`
-    promises.push(fetch(url).then((res) => res.json())); //Stores all promises(pokemon) into an array.
-  } 
-  Promise.all(promises).then((results) => { //Converts it to a format that loads all Pokemon at one time as opposed to having each loop run one after the other.
+  for (let i = 252; i <= 385; i++) {
+    const url = `https://pokeapi.co/api/v2/pokemon/${i}`;
+    promises.push(fetch(url).then((res) => res.json()));
+    // Stores all promises(pokemon) into an array.
+  }
+  Promise.all(promises).then((results) => {
+    // Converts it to a format that loads all Pokemon at one time as opposed
+    // to having each loop run one after the other.
     const pokemon3 = results.map((data) => ({
-            name: data.name,
-            id: data.id,
-            image: data.sprites['front_default'],
-            shiny: data.sprites['front_shiny'],
-            type: data.types.map((type) => type.type.name).join(', ')
+      name: data.name,
+      id: data.id,
+      image: data.sprites.front_default,
+      shiny: data.sprites.front_shiny,
+      type: data.types.map((type) => type.type.name).join(' '),
     }));
-    console.log(pokemon3)
+    renderPokemon(pokemon3);
   });
 };
 
-genThreePokemon();
+// Event listener for Generation Selection
+const selector = document.getElementById('genselector');
 
+selector.addEventListener('change', (event) => {
+  if (selector.value === 'gen1') {
+    return genOnePokemon();
+  }
+  if (selector.value === 'gen2') {
+    return genTwoPokemon();
+  }
+  if (selector.value === 'gen3') {
+    return genThreePokemon();
+  }
+  return true;
+});
 
+typeOne.addEventListener('change', (e) => {
+  const typeOneValue = e.target.value;
 
+  if (selector.value === 'gen1') {
+    return genOnePokemon(typeOneValue);
+  }
+  if (selector.value === 'gen2') {
+    return genTwoPokemon(typeOneValue);
+  }
+  if (selector.value === 'gen3') {
+    return genThreePokemon(typeOneValue);
+  }
+});
 
+function loopCards() {
+  for (let i = 0; i <= 385; i++) {}
+}
 
+loopCards();
 
+const searchInput = document.getElementById('search');
+function searchPokemon(event) {
+  const input = event.target.value.toLowerCase();
 
-// testFunction() 
+  let cards = document.getElementsByClassName('card-title');
+  cards = Array.from(cards); // cards = [...cards];
+
+  cards.forEach((el) => {
+    // console.log(el.textContent.toLowerCase().includes(input));
+    if (!el.textContent.toLowerCase().includes(input)) {
+      el.parentElement.style.display = 'none';
+    } else {
+      el.parentElement.style.display = '';
+    }
+  });
+}
+searchInput.addEventListener('keyup', searchPokemon);
+// searchPokemon();
+// backgroundColour.addEventListener('change', event => {
+//   if (backgroundColour.indexOf(colour)  )
+// })
+
+// // function searchFilter() {
+//   // Declare variables
+//   var input, filter, ul, li, a, i, txtValue;
+//   input = document.getElementById('search');
+//   filter = input.value.toUpperCase();
+//   ol = document.getElementById("pokedex");
+//   li = ol.getElementsByTagName('li');
+
+//   // Loop through all list items, and hide those who don't match the search query
+//   for (i = 0; i < li.length; i++) {
+//     h2 = li[i].getElementsByTagName("h2")[0];
+//     txtValue = a.textContent || h2.innerText;
+//     if (txtValue.toUpperCase().indexOf(filter) > -1) {
+//       li[i].style.display = "";
+//     } else {
+//       li[i].style.display = "none";
+//     }
+//   }
+// }
+
+// searchFilter();
+
+// testFunction()
 // const promises = [];
 //   for(let i=152; i<=gen2; i++) {
 //     const url = `https://pokeapi.co/api/v2/pokemon/${i}`
 //     promises.push(fetch(url).then((res) => res.json())); //Stores all promises(pokemon) into an array.
-//   } 
+//   }
 //   Promise.all(promises).then((results) => { //Converts it to a format that loads all Pokemon at one time as opposed to having each loop run one after the other.
 //     const pokemon = results.map((data) => ({
 //             name: data.name,
@@ -113,7 +216,7 @@ genThreePokemon();
 //   for(let i=start; i<=stop; i++) {
 //     const url = `https://pokeapi.co/api/v2/pokemon/${i}`
 //     promises.push(fetch(url).then((res) => res.json())); //Stores all promises(pokemon) into an array.
-//   } 
+//   }
 //   return await Promise.all(promises).then((results) => { //Converts it to a format that loads all Pokemon at one time as opposed to having each loop run one after the other.
 //     pokemon = results.map((data) => ({
 //             name: data.name,
@@ -125,7 +228,7 @@ genThreePokemon();
 //     // console.log(pokemon)
 //     return pokemon
 //   });
-  
+
 // }
 // console.log(testData(GENERATION.ONE));
 // console.log(testData(GENERATION.TWO));
